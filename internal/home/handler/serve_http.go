4bcb50fe2860aecd2299f00e0b95d92b61909f11
Ryan Noronha <35768724+waduhek@users.noreply.github.com>
2025-11-13 17:42:49 +0000

    refactor(server): add span attributes to the home page span (#87)
    Adds some span attributes recommended by semantic conventions to the home page
    span.

--- internal/home/handler/serve_http.go
+++ internal/home/handler/serve_http.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/otel/codes"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 
 	"github.com/waduhek/website/internal/telemetry"
 	"github.com/waduhek/website/internal/templates"
@@ -13,15 +14,22 @@ import (
 func (h *HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := telemetry.ExtractContext(r.Context(), r.Header)
 
-	_, span := telemetry.NewSpan(ctx, "GET /")
+	spanCtx, span := telemetry.NewSpan(ctx, "GET /")
 	defer span.End()
+
+	span.SetAttributes(
+		semconv.HTTPRequestMethodGet,
+		semconv.URLPath(r.URL.Path),
+		semconv.URLScheme(r.URL.Scheme),
+	)
 
 	template := h.templateService.GetTemplate(templates.Home)
 	if template == nil {
-		h.logger.ErrorContext(ctx, "did not get the template for home page")
+		h.logger.ErrorContext(spanCtx, "did not get the template for home page")
 
 		span.AddEvent("got nil template, returning status 500")
 		span.SetStatus(codes.Error, "got nil template")
+		span.SetAttributes(semconv.HTTPResponseStatusCode(500))
 
 		w.WriteHeader(500)
 		fmt.Fprint(w, "oops an error occurred")
@@ -31,5 +39,6 @@ func (h *HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	template.Execute(w, nil)
 
+	span.SetAttributes(semconv.HTTPResponseStatusCode(200))
 	span.SetStatus(codes.Ok, "home template found and executed successfully")
 }
