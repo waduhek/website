bec5d85573cafefa1c40310ccf70f89aa08d34b1
Ryan Noronha <ryannor56a@proton.me>
2025-11-13 21:54:46 +0530

    refactor(server): remove unwanted methods from experience repository
    Removes all methods apart from GetAll from the experience repository.

--- internal/experience/repository/internal/delete_all.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-import "context"
-
-func (r *ExperiencePgxRepository) DeleteAll(ctx context.Context) error {
-	query := "TRUNCATE TABLE experience;"
-	_, err := r.dbConn.Exec(ctx, query)
-	return err
-}
