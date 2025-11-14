29df739fcb1edc1f078fb9b82871fcb85200a807
Ryan Noronha <ryannor56a@proton.me>
2025-11-14 23:29:05 +0530

    refactor(server): removes unwanted methods from the education repository

--- internal/education/repository/interface.go
+++ internal/education/repository/interface.go
@@ -7,25 +7,7 @@ import (
 )
 
 type EducationRepository interface {
-	// InsertOne inserts a single education entry.
-	InsertOne(ctx context.Context, education *models.EducationInputModel) error
-
-	// InsertMany inserts multiple education entries.
-	InsertMany(
-		ctx context.Context,
-		educations ...models.EducationInputModel,
-	) error
-
 	// GetAll gets all education entries. Sorts the entries in descending order
 	// of their start dates.
 	GetAll(ctx context.Context) ([]models.EducationOutputModel, error)
-
-	// GetByID gets an education by it's ID.
-	GetByID(ctx context.Context, id int32) (*models.EducationOutputModel, error)
-
-	// DeleteAll deletes all education entries.
-	DeleteAll(ctx context.Context) error
-
-	// DeleteByID deletes an education entry by it's ID.
-	DeleteByID(ctx context.Context, id int32) error
 }
