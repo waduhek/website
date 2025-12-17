aa7fd79d86512dfdef41ec3c1a9c51af97e0e2d2
Ryan Noronha <35768724+waduhek@users.noreply.github.com>
2025-12-17 17:44:41 +0000

    fix(server): update the buckets for http request duration metric (#132)
    Updates the buckets used for HTTP request duration metrics to the buckets
    recommended by OTel semantic conventions.

--- internal/telemetry/middlewares.go
+++ internal/telemetry/middlewares.go
@@ -18,7 +18,13 @@ type TelemetryCollector struct {
 func NewTelemetryCollector(
 	meter metric.Meter,
 ) (*TelemetryCollector, error) {
-	requestDurationMeter, err := httpconv.NewServerRequestDuration(meter)
+	requestDurationMeter, err := httpconv.NewServerRequestDuration(
+		meter,
+		metric.WithExplicitBucketBoundaries(
+			0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5,
+			7.5, 10,
+		),
+	)
 	if err != nil {
 		return nil, err
 	}
